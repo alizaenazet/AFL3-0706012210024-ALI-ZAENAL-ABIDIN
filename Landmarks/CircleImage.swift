09dc8e7d5ad69cc2d9bd124241f34811f9b2d05f
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-25 23:49:18 +0700

    finish:C.1-P.2-S.7

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -8,16 +8,13 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
-            .clipShape(Circle())
-            .overlay {
-                Circle().stroke(.white, lineWidth: 4)
-            }
-            .shadow(radius: 7)
+        image
     }
 }
 
 #Preview {
-    CircleImage()
+    CircleImage(image: Image("turtlerock"))
 }
