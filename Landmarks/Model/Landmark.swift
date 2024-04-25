f1d3ef32b763aa1ca94923aeb68fa13387df6d62
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-25 17:49:44 +0700

    finish:C.1-P.2-S.1

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,38 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Alizaenal Abidin on 25/04/24.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation //
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var state: String
+    var park: String
+    var description: String
+    
+    // Solusi guna menyertakan sebuah component pada model
+    private var imageName: String
+    var image: Image { // clouse dengan return component `Image` SwiftUi
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+
+}
