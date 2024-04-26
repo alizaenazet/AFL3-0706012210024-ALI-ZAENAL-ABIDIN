e177bf50253a939dfe19966a1524eacaf240973c
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-26 17:31:02 +0700

    finish:C.3-P.2-S.1

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Alizaenal Abidin on 26/04/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+            VStack(alignment: .leading, spacing: 20) {
+                ProfileSummary(profile: draftProfile)
+            }
+            .padding()
+        
+        }
+    
+}
+
+#Preview {
+    ProfileHost()
+}
