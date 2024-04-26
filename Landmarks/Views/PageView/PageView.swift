5bf06b1c1b3fc05905446422623233e20563ba0f
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-26 23:32:24 +0700

    finish:C.4-P.1-S.2

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
-
     var body: some View {
         PageViewController(pages: pages)
             .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
+
 #Preview {
     PageView(
         pages: ModelData().features.map {
